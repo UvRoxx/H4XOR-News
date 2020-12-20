444e8678fc7782780f96d1c8f70b7457c29bb1dc
Utkarsh Varma <uvroxx2323@gmail.com>
2020-12-19 21:31:15 -0500

    Initial Commit

---
+++ H4XOR News/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  H4XOR News
+//
+//  Created by UTKARSH VARMA on 2020-12-19.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
